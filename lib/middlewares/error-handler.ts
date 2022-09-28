ac1a15a6e46ae0c02b5963b571af0b29c252ece5
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2022-09-28 08:05:21 +0530

    removed console logs

--- lib/middlewares/error-handler.ts
+++ lib/middlewares/error-handler.ts
@@ -2,13 +2,11 @@ import type { Request, Response, NextFunction } from 'express';
 import { CustomError } from '../errors';
 import { httpCodes } from '../constants/http-status-codes';
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log('Error-handlder-called');
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({
       errors: err.serializeErrors()
     });
   }
-  console.error('ggggg', err);
   res.status(httpCodes.badRequest).send({
     errors: [
       {
